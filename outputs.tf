19b78b17c10f7c7385af9f239b642ad27c1f26ca
jerpen80 <dra_penny@yahoo.com>
2026-06-04 11:16:42 +0200

    removed unused outputs

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,3 @@
-output "lambda_cloudwatch_alarm_creator_arn" {
-  value = module.lambda_cw_alarm_creator.lambda_function_arn
-}
-
-output "lambda_cloudwatch_alarm_creator_name" {
-  value = module.lambda_cw_alarm_creator.lambda_function_name
-}
-
 output "lambda_payload_forwarder_arn" {
   value = module.lambda_payload_forwarder.lambda_function_arn
 }
